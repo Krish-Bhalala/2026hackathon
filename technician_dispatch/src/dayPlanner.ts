9fedd2ab055c904925189d7a59093ad6347fbc3e
Krish Bhalala <145704986+Krish-Bhalala@users.noreply.github.com>
2026-03-14 10:55:31 -0500

    Implemented calculateRouteDuration()

--- technician_dispatch/src/dayPlanner.ts
+++ technician_dispatch/src/dayPlanner.ts
@@ -59,7 +59,7 @@ export class DayPlanner {
     haversineDistance(loc1: Location, loc2: Location): number {
         const R = 6371;
         const toRad = (deg: number) => (deg * Math.PI) / 180;
-        const dLat = toRad(loc2.latitude  - loc1.latitude);
+        const dLat = toRad(loc2.latitude - loc1.latitude);
         const dLng = toRad(loc2.longitude - loc1.longitude);
         const lat1 = toRad(loc1.latitude);
         const lat2 = toRad(loc2.latitude);
@@ -84,12 +84,62 @@ export class DayPlanner {
         boxes: Box[],
         routeIds: string[]
     ): number | null {
-        // TODO: implement this method
-        throw new Error('Not implemented');
+        if (!technician || !boxes) {
+            return null;
+        }
+        if (!this.validateTechnician(technician)) {
+            return null;
+        }
+
+        // Empty route is valid returns 0
+        if (routeIds.length === 0) {
+            return 0;
+        }
+
+        // Build a dictionary for boxes
+        const boxMap = new Map<string, Box>();
+        for (const box of boxes) {
+            if (this.validateBox(box)) {
+                boxMap.set(box.id, box);
+            }
+        }
+
+        let totalDuration = 0;
+        let prevLocation: Location = technician.startLocation;
+
+        for (let i = 0; i < routeIds.length; i++) {
+            const box = boxMap.get(routeIds[i]);
+            if (!box) {
+                return null;
+            }
+            // Travel from previous location to this box
+            totalDuration += this.travelTimeMinutes(prevLocation, box.location, technician.speedKmh);
+            // Fix the box
+            totalDuration += box.fixTimeMinutes;
+            prevLocation = box.location;
+        }
+
+        return totalDuration;
     }
 
     planDay(technician: Technician, boxes: Box[]): DayPlanResult {
         // TODO: implement this method
         throw new Error('Not implemented');
     }
-}
+
+    validateTechnician(technician: Technician): boolean {
+        return technician.id !== undefined && technician.name !== undefined && technician.startLocation !== undefined && technician.speedKmh !== undefined && technician.workingMinutes !== undefined;
+    }
+
+    validateBox(box: Box): boolean {
+        return box.id !== undefined && box.name !== undefined && box.location !== undefined && box.fixTimeMinutes !== undefined;
+    }
+
+    validateRouteIds(routeIds: string[]): boolean {
+        return routeIds.length >= 0;
+    }
+
+    validateBoxes(boxes: Box[]): boolean {
+        return boxes.length > 0 && boxes.every((box) => this.validateBox(box));
+    }
+}   
