cb60bcc53f97ca2e186da58d6c174c2444c69ce3
lukewiebe <mail@lukewiebe.com>
2026-03-14 11:06:24 -0500

    Complete calculateRouteDistance

--- technician_dispatch/src/routeOptimizer.ts
+++ technician_dispatch/src/routeOptimizer.ts
@@ -67,8 +67,19 @@ export class RouteOptimizer {
         boxes: Box[],
         routeIds: string[]
     ): number | null {
-        // TODO: implement this method
-        throw new Error('Not implemented');
+        if (routeIds.length === 0) return 0
+
+        // build a list of locations
+        const locations = routeIds.map((id) => boxes.find((box) => box.id === id)?.location)
+        locations.unshift(technician.startLocation)
+        if (!locations.every((x): x is Location => x !== undefined)) return null
+
+        let sum = 0
+        for (let i = 0; i < locations.length - 1; i++) {
+            sum += this.haversineDistance(locations[i], locations[i + 1])
+        }
+
+        return sum
     }
 
     findShortestRoute(technician: Technician, boxes: Box[]): RouteResult {
